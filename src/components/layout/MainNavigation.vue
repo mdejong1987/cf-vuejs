abc1678894d457ee56ebff472ac038abc633cd0d
M. de Jong <dej.michael@hotmail.com>
2023-10-28 22:37:48 +0200

    added vue router

--- src/components/layout/MainNavigation.vue
+++ src/components/layout/MainNavigation.vue
@@ -1,18 +1,10 @@
 <template>
-    <div class="row">
-        <div class="columns">
-            <h1>Logo</h1>
-        </div>
-        <div class="columns">
-            <ul class="menu">
-                <li><a href="">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Portfolio</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </div>
-    </div>
-
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Portfolio</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
 </template>
 
 <script>
