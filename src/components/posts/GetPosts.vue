9f1661b2799282fce731d941f5d444a1cbbacb7f
Michael de Jong <dej.michael@hotmail.com>
2023-10-30 14:28:43 +0100

    added some template checks

--- src/components/posts/GetPosts.vue
+++ src/components/posts/GetPosts.vue
@@ -1,20 +1,19 @@
 <template>
-    {{ wpPosts.status }}
-    {{ wpPosts.type }}
-
     <div v-for="post in wpPosts" v-bind:key="post.id" class="animate__animated animate__fadeIn">
+      <div v-if="(post.status === 'publish') && (post.type === 'post')">
         <h2>{{ post.title.rendered }}</h2>
         <div v-html="post.excerpt.rendered"></div>
+      </div>
     </div>
 </template>
 
 <script>
-export default {
-  name: 'GetPosts',
-  props: ['wpPosts'],
-  data() {
-    return {
-    }
-  },
-}
+  export default {
+    name: 'GetPosts',
+    props: ['wpPosts'],
+    data() {
+      return {
+      }
+    },
+  }
 </script>
