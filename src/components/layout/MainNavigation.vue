7da21cfd6fb4f0bf14f8738f6c620487f17aa4e4
Michael de Jong <dej.michael@hotmail.com>
2023-11-15 16:26:01 +0100

    added small fixes

--- src/components/layout/MainNavigation.vue
+++ src/components/layout/MainNavigation.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="navbar-fixed">
-        <nav>
+        <nav class="nav-transparent">
             <div class="nav-wrapper">
                 <div class="container">
                     <a href="/" class="brand-logo">Logo</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <ul id="nav-mobile" class="right">
                         <li><router-link to="/">Home</router-link></li>
                         <li v-for="page in pages" :key="page.id">
                             <router-link :to="`/${page.slug}`">{{ page.title.rendered
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -8,18 +8,13 @@
   </section>
 
   <div class="container">
-    <section class="card blue-grey darken-1">
+    <section class="card teal lighten-3">
       <div class="card-content white-text">
-        <h2 class="header">Test</h2>
-        <p class="flow-text">
+        <h2 class="header center-align">Test</h2>
+        <p class="flow-text center-align">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus libero enim, suscipit at justo sit amet,
+          lobortis. Vivamus libero enim, suscipit at justo sit amet,
           lobortis
-          mollis eros. Nam ut viverra diam. Ut volutpat at erat aliquam pellentesque. Morbi vulputate pretium vestibulum.
-          Phasellus interdum eleifend lorem, ac rhoncus risus. Nunc vestibulum commodo mi vulputate semper. In nisl
-          lectus,
-          rutrum nec rutrum sit amet, posuere eu urna. Nullam eget varius quam, sed pretium odio. Donec ex eros, tristique
-          non
-          semper vitae, commodo in magna.
         </p>
       </div>
     </section>
